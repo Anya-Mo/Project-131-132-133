40895167b3b0c3867a856a502ba7274fbbd96477
Anya-Mo <72193720+Anya-Mo@users.noreply.github.com>
2022-05-29 16:12:44 +0800

    Add files via upload

---
+++ bedroom.js
@@ -0,0 +1,41 @@
+image_bedroom="";
+status_object="";
+objects=[];
+function preload() {
+    image_bedroom=loadImage("bedroom.png");
+}
+function setup() {
+    canvas=createCanvas(600,400);
+    canvas.position(345,175);
+    object_detector=ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("status").innerHTML="Status: Detecting Objects";
+}
+function draw() {
+    if (status_object != "") {
+        image(image_bedroom,0,0,600,400);
+        for (var i=0;i<objects.length;i++) {
+            document.getElementById("status").innerHTML="Status: Objects Detected";
+            fill("black");
+            percentage=floor(objects[i].confidence*100);
+            console.log(percentage);
+            text(objects[i].label+" "+percentage+"%",objects[i].x+5,objects[i].y+15);
+            noFill();
+            stroke("black");
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+        }
+    }
+}
+function modelLoaded() {
+    console.log("Model Loaded");
+    status_object=true;
+    object_detector.detect(image_bedroom,gotResults);
+}
+function gotResults(error,results) {
+    if (error) {
+        console.log(error);
+    }
+    else {
+        console.log(results);
+        objects=results;
+    }
+}
